8c717710a0efa720569ceed5998911d989e2b79d
sDEV-eloper <thisissurajdev@gmail.com>
2023-08-24 00:13:58 +0530

    conversation ui added

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -6,13 +6,13 @@ import {  Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import Sidebar from './sidebar';
 
 const MobileSidebar = () => {
-  // const [isMounted, setIsMounted]=useState(false)
-  // useEffect(()=>{
-  //   setIsMounted(true)
-  // },[])
-  // if(!isMounted){
-  //   return null
-  // }
+  const [isMounted, setIsMounted]=useState(false)
+  useEffect(()=>{
+    setIsMounted(true)
+  },[])
+  if(!isMounted){
+    return null
+  }
   return (
     
       <Sheet>
