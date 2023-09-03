48f2c1af1eeb29e59210cd3dee91da5471ba7228
sDEV-eloper <thisissurajdev@gmail.com>
2023-09-03 23:05:59 +0530

    added landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,24 +1,91 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import Logo from "@/public/images/logo4.png"
+import Image from "next/image";
+import { feature } from "@/components/utils/constants";
  
 export default function LandingPage() {
+ 
   return (
     <div>
-        Landing Page
-        <div>
-        <Link href="/sign-in">
-        <Button>
+      {/* <!-- Section: Design Block --> */}
+      <section className="mb-40 ">
+        {/* <!-- Navbar --> */}
+        <nav
+          className="relative flex w-full items-center justify-between bg-white py-2 shadow-sm shadow-neutral-700/10 dark:bg-neutral-800 dark:shadow-black/30 lg:flex-wrap lg:justify-start"
+          data-te-navbar-ref>
+          {/* <!-- Container wrapper --> */}
+          <div className="flex w-full flex-wrap items-center justify-between px-6">
+            <div className="flex items-center">
+              {/* <!-- Toggle button --> */}
+              <button
+                className="block border-0 bg-transparent py-2 pr-2.5 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
+                type="button" data-te-collapse-init data-te-target="#navbarSupportedContentY"
+                aria-controls="navbarSupportedContentY" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="[&>svg]:w-7">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-7 w-7">
+                    <path fill-rule="evenodd"
+                      d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                      clip-rule="evenodd" />
+                  </svg>
+                </span>
+              </button>
+
+              {/* <!-- Navbar brand --> */}
+              <Link className="text-primary dark:text-primary-400" href="#!">
+                <div className="[&>svg]:ml-2 [&>svg]:mr-3 [&>svg]:h-6 [&>svg]:w-6 [&>svg]:lg:ml-0">
+                <div className="flex justify-center items-center gap-2">
+                <Image src={Logo} alt="logo" width={30}/>
+                 <p className="text-xl font-bold">Mediai</p>
+                </div>
+                </div>
+              </Link>
+            </div>
+
+            {/* <!-- Collapsible wrapper --> */}
+           
+            
+            <div className="my-1 flex items-center lg:my-0 lg:ml-auto">
+              <Link href="/sign-in">
+              <button type="button"
+                className="mr-2 inline-block rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:text-primary-400 dark:hover:bg-neutral-700 dark:hover:bg-opacity-60 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+                data-te-ripple-init data-te-ripple-color="light">
           Login
-        </Button>
+              </button>
         </Link>
+              
         <Link href="/sign-up">
-        <Button>
-          Sign up
-        </Button>
-        </Link>
-      </div>
+              <button type="button"
+                className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                data-te-ripple-init data-te-ripple-color="light">
+                Sign up 
+              </button>
+                </Link>
+            </div>
+       
+          </div>
       
+        </nav>
+     
+        <div className="bg-gradient-to-b from-gray-700 via-gray-900 to-black py-24 px-6 text-center dark:bg-neutral-900">
+          <h1 className="mt-2 text-white mb-16 text-5xl font-bold tracking-tight md:text-5xl xl:text-5xl">
+          Transform Your Ideas into Impactful  <br /><span className="text-primary text-4xl text-gray-400">Content with the Power of OpenAI</span>
+          </h1>
+          <Link className="border hover:text-gray-300 mb-2 inline-block rounded bg-primary px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
+            data-te-ripple-init data-te-ripple-color="light" href="/dashboard" role="button">Get started</Link>
+        </div>
+
+      <div className="flex p-4 justify-center items-center ">
+      {
+        feature.map((item)=>
+        <p key={item.name} className="text bold border rounded-lg p-3 text-white bg-[#292929] ">
+          {item.name}
+          </p>)
+      }
+      </div>
+      </section>
+      {/* <!-- Section: Design Block --> */}
     </div>
   )
 }
